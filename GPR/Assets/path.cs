c414439766239803f033ba995371748364283030
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-09-24 13:24:30 +0200

    updated

--- GPR/Assets/path.cs
+++ GPR/Assets/path.cs
@@ -2,28 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class path : MonoBehaviour
-{
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _speed;
-    [SerializeField] private float _arrival = 0.1f;
-    int waypointIndex = 0;
+public class Path : MonoBehaviour {
 
-    private void Update()
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.3f; 
+    private int lastWaypointIndex;
+
+    public float movementSpeed;
+    public float rotationSpeed;
+
+	void Start () {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoint = waypoints[targetWaypointIndex]; 
+	}
+	
+	void Update () {
+        float movementStep = movementSpeed * Time.deltaTime;
+        float rotationStep = rotationSpeed * Time.deltaTime;
+
+        Vector3 directionToTarget = targetWaypoint.position - transform.position;
+        Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget); 
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationStep); 
+
+        Debug.DrawRay(transform.position, transform.forward * 50f, Color.green, 0f); //Draws a ray forward in the direction the enemy is facing
+        Debug.DrawRay(transform.position, directionToTarget, Color.red, 0f); //Draws a ray in the direction of the current target waypoint
+
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWaypoint(distance);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
+	}
+
+    void CheckDistanceToWaypoint(float currentDistance)
     {
-        Vector3 heightOffsetPosition = new Vector3(_target.position.x, transform.position.y, _target.position.z);
-        float distance = Vector3.Distance(transform.position, heightOffsetPosition);
-
-
-        if (distance <= _arrival)
+        if(currentDistance <= minDistance)
         {
-            print("Ik ben er!");
+            targetWaypointIndex++;
+            UpdateTargetWaypoint();
         }
-        else
-        {
-            transform.LookAt(heightOffsetPosition);
-            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-        
-        }
+    }
+
+    void UpdateTargetWaypoint()
+    {
+        targetWaypoint = waypoints[targetWaypointIndex];
     }
 }
