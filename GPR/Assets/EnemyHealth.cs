ad99ab64b9ad2bde87cc97f73155944652d36e90
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-10-23 23:55:10 +0200

    Player Feedback

---
+++ GPR/Assets/EnemyHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public float damage;
+    public float lifetime = 1f;
+
+    public GameObject healthbarUI;
+    public Animation animateFeedback;
+    public Slider slider1;
+    public Slider slider2;
+
+    void Start()
+    {
+        health = maxHealth;
+        slider1.value = CalculateHealth();
+        slider2.value = CalculateHealth();
+        healthbarUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            health -= damage;
+            animateFeedback.Play("DMGAnim");
+        }
+
+        slider1.value = CalculateHealth();
+        slider2.value = CalculateHealth();
+
+        if (health < maxHealth)
+        {
+            healthbarUI.SetActive(true);
+        }
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if(health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    float CalculateHealth()
+    {
+        return health / maxHealth;
+    }
+}
---
+++ GPR/Assets/destroyArrow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyArrow : MonoBehaviour
+{
+    public float lifetime = 10f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
